980f9c30c384f7ba7dc25510bde5b355fe7c3205
ShanwazWaqar <ShanwazWaqarKotekanti@gmail.com>
2022-09-26 19:16:36 -0400

    basic setup with structuring

--- MovieBookingSystem/src/app/app-routing.module.ts
+++ MovieBookingSystem/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from '../app/user/user.component';
+import { AdminComponent } from './admin/admin.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '/admin', component: AdminComponent },
+  { path: '**', component: UserComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MovieBookingSystem/src/app/app.module.ts
+++ MovieBookingSystem/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    UserComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
