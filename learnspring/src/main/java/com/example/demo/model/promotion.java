8efd25c5ab632793a4353e2d0b78fe709a3f54f4
Naveen Kurra <70699929+naveen-kurra@users.noreply.github.com>
2022-11-06 16:05:58 -0500

    Add files via upload

---
+++ learnspring/src/main/java/com/example/demo/model/promotion.java
@@ -0,0 +1,90 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
+@ToString
+@Table(name="promotion")
+public class promotion {
+	@Id
+	@GeneratedValue
+	private int pid;
+	private String old_pcode;
+	public String getOld_pcode() {
+		return old_pcode;
+	}
+	public void setOld_pcode(String old_pcode) {
+		this.old_pcode = old_pcode;
+	}
+	public promotion() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public promotion(int pid, int Aid, String pcode, String start, String end, double percentage,String old_pcode) {
+		super();
+		this.pid = pid;
+		this.Aid = Aid;
+		this.pcode = pcode;
+		this.start = start;
+		this.end = end;
+		this.percentage = percentage;
+		this.old_pcode=old_pcode;
+	}
+	@Override
+	public String toString() {
+		return "promotion [pid=" + pid + ", Aid=" + Aid + ", pcode=" + pcode + ", start=" + start + ", end=" + end
+				+ ", percentage=" + percentage + "]";
+	}
+	private int Aid;
+	private String pcode;
+	private String start;
+	private String end;
+	private double percentage;
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public int getAid() {
+		return Aid;
+	}
+	public void setAid(int aid) {
+		Aid = aid;
+	}
+	public String getPcode() {
+		return pcode;
+	}
+	public void setPcode(String pcode) {
+		this.pcode = pcode;
+	}
+	public String getStart() {
+		return start;
+	}
+	public void setStart(String start) {
+		this.start = start;
+	}
+	public String getEnd() {
+		return end;
+	}
+	public void setEnd(String end) {
+		this.end = end;
+	}
+	public double getPercentage() {
+		return percentage;
+	}
+	public void setPercentage(double percentage) {
+		this.percentage = percentage;
+	}
+}
---
+++ learnspring/src/main/java/com/example/service/imple/Emailsenderservice.java
@@ -0,0 +1,39 @@
+package com.example.service.imple;
+
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Emailsenderservice {
+		
+		@Autowired
+		private JavaMailSender javaMailSender;
+		public void sendemailwithattachment(String toEmail,String body, String subject) throws MessagingException
+		{
+			
+			
+			MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+			MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+			mimeMessageHelper.setFrom("noreply.bookmyshow99@gmail.com");
+			mimeMessageHelper.setTo(toEmail);
+			mimeMessageHelper.setText(body);
+			mimeMessageHelper.setSubject(subject);
+			
+			//FileSystemResource filer= new FileSystemResource(new File(attachement));
+			//mimeMessageHelper.addAttachment(filer.getFilename(), filer);
+			javaMailSender.send(mimeMessage);
+			System.out.println("Mail sent");
+		}
+
+	}
+
