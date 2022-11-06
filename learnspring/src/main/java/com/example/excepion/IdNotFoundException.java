0b571ad504c6858076f799cb78ae8bded7f802f6
Naveen Kurra <70699929+naveen-kurra@users.noreply.github.com>
2022-11-06 16:39:53 -0500

    Delete learnspring directory

--- learnspring/src/main/java/com/example/excepion/IdNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package com.example.excepion;
-@SuppressWarnings("serial")
-public class IdNotFoundException extends Exception {
-	public IdNotFoundException(String message) {
-		super(message);
-}}
