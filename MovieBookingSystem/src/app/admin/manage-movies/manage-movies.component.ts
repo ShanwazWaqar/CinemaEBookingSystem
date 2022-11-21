0ebdcea49d05166a2895990b23edbe5c459f7069
unknown <nkurra7@gmail.com>
2022-11-20 23:43:09 -0500

    manage-movies-display-admin

--- MovieBookingSystem/src/app/admin/manage-movies/manage-movies.component.ts
+++ MovieBookingSystem/src/app/admin/manage-movies/manage-movies.component.ts
@@ -1,6 +1,7 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialog, MAT_DIALOG_DATA  } from '@angular/material/dialog';
+import { bmsApiService } from 'src/app/services/bmsapi.service';
 import { EditMovieComponent } from '../edit-movie/edit-movie.component'
 @Component({
   selector: 'app-manage-movies',
@@ -13,63 +14,25 @@ export class ManageMoviesComponent implements OnInit {
   archiveMoviesSelected:boolean = false;
   scheduleMoviesSelected:boolean = false;
 
-  movies = [
-    {
-      id: 1,
-      title: "Baby Driver",
-      image: '../../../assets/images/movie1.jpg',
-    },
-    {
-      id: 2,
-      title: "Bohemain",
-      image: '../../../assets/images/movie2.jpg',
-    },
-    {
-      id: 3,
-      title: "Black Adam",
-      image: '../../../assets/images/movie3.webp',
-    },
-    {
-      id: 4,
-      title: "West Side Story",
-      image: '../../../assets/images/movie4.jpg',
-    },
-    {
-      id: 5,
-      title: "The Godfather",
-      image: '../../../assets/images/movie5.jpg',
-    },
-    {
-      id: 6,
-      title: "Hangover II",
-      image: '../../../assets/images/movie6.jpg',
-    },
-    // {
-    //   id: 7,
-    //   title: "title1",
-    //   image: '../../../assets/images/movie7.jpg',
-    // },
-    // {
-    //   id: 8,
-    //   title: "title1",
-    //   image: '../../../assets/images/movie8.jpg',
-    // },
-    // {
-    //   id: 9,
-    //   title: "title1",
-    //   image: '../../../assets/images/movie9.webp',
-    // },
-    // {
-    //   id: 10,
-    //   title: "title1",
-    //   image: '../../../assets/images/movie10.jpg',
-    // },
-  ];
-  constructor(private dialogRef: MatDialog) { }
+  movies:any ;
+  constructor(private dialogRef: MatDialog, private bms: bmsApiService) { }
 
   ngOnInit(): void {
     this.currentMoviesSelected = true;
     // this.addmoviesSelected = true;
+    this.bms.getMoviesList().subscribe(res=>{
+      console.log(res," res of movies list");
+      this.movies = [];
+      for(let i=0;i<res.length;i++) {
+        let obj = {
+          title: res[i].title,
+          image: res[i].thumbnail,
+          status: res[i].currentrunning == 1 ? "Currently Playing" : "Upcoming Soon"
+        };
+        this.movies.push(obj);
+      }
+      console.log(this.movies," movies list");
+    });
   }
 
   addMovies() {
