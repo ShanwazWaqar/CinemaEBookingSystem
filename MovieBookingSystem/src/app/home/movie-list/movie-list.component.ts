81d6cbec134b2a7db88f05bf75f1861dab9b3f76
unknown <nkurra7@gmail.com>
2022-11-22 01:21:12 -0500

    filter

--- MovieBookingSystem/src/app/home/movie-list/movie-list.component.ts
+++ MovieBookingSystem/src/app/home/movie-list/movie-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, HostListener, Input, OnInit } from '@angular/core';
-import {CarouselModule} from 'primeng/carousel';
-import { MatDialog, MAT_DIALOG_DATA  } from '@angular/material/dialog';
+import { CarouselModule } from 'primeng/carousel';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { PopupTraierComponent } from '../popup-traier/popup-traier.component';
 import { Router } from '@angular/router';
 import { bmsApiService } from 'src/app/services/bmsapi.service';
@@ -12,62 +12,83 @@ import { MsgPopupHomeComponent } from '../msg-popup-home/msg-popup-home.componen
   styleUrls: ['./movie-list.component.scss']
 })
 export class MovieListComponent implements OnInit {
-  responsiveOptions:any;
-  searching:boolean = true;
-  searchComp:boolean = false;
-  currentPlaying:boolean = false;
-  currentMovies:any = [];
-  upcomingMovies:any = [];
-  @Input() searchText = ''; 
-  
-  constructor( private dialogRef: MatDialog,private router: Router, private bms:bmsApiService) { 
+  genres = ["Action", "Horror", "Thriller", "Comedy", "Drama", "Adventure", "Documentary", "Fiction", "Mystery", "Animation"];
+  responsiveOptions: any;
+  searching: boolean = true;
+  searchComp: boolean = false;
+  currentPlaying: boolean = false;
+  currentMovies: any = [];
+  upcomingMovies: any = [];
+  tempCurrentMovies: any = [];
+  tempUpcomingMovies: any = [];
+  totalMovies: any = [];
+  @Input() searchText = '';
+  count = 0;
+  currentFilterCategory = "";
+  upcomingFilterCategory = "";
+
+  constructor(private dialogRef: MatDialog, private router: Router, private bms: bmsApiService) {
     this.responsiveOptions = [{
       breakpoint: '1024px',
       numVisible: 1,
       numScroll: 3
-  }];
+    }];
+  }
+
+  filterMovies() {
+    for (let i = 0; i < this.totalMovies.length; i++) {
+      this.totalMovies[i].rating2 = (this.totalMovies[i].rating.split(":"))[0];
+      if (this.totalMovies[i].currentrunning == 1) {
+        this.currentMovies.push(this.totalMovies[i]);
+      } else {
+        this.upcomingMovies.push(this.totalMovies[i]);
+      }
+    }
   }
 
   ngOnInit(): void {
-    this.bms.getMoviesList().subscribe(res=>{
-      for(let i=0;i<res.length;i++) {
-        if(res[i].currentrunning == 1) {
-          res[i].rating2 = (res[i].rating.split(":"))[0];
-          this.currentMovies.push(res[i]);  
-        }else {
-          this.upcomingMovies.push(res[i]);
-        }
-      }
+    this.bms.getMoviesList().subscribe(res => {
+      this.totalMovies = res;
+      this.filterMovies();
+      this.tempCurrentMovies = this.currentMovies;
+      this.tempUpcomingMovies = this.upcomingMovies;
       this.searching = false;
     });
   }
 
-  ngOnChanges(){
-    if(this.searchText){
-      this.searching = false;
-      this.searchComp = true;
-    } else { 
-      this.searching = true;
-      this.searchComp = false;
+  ngOnChanges() {
+    if (this.count == 0) {
+      if (this.searchText) {
+        this.searching = false;
+      } else {
+        this.searching = true;
+      }
+    } else {
+      if (this.searchText) {
+        this.searching = true;
+      } else {
+        this.searching = false;
+      }
     }
+    this.count++;
   }
 
-  openTrailerPopup(link:any) {
-    
-    link=link.split("=",);
-    link=link[1]
+  openTrailerPopup(link: any) {
+
+    link = link.split("=",);
+    link = link[1]
     console.log(link)
     const popup = this.dialogRef.open(PopupTraierComponent, {
       disableClose: true,
       enterAnimationDuration: '700ms',
-      exitAnimationDuration:'1000ms',
+      exitAnimationDuration: '1000ms',
       maxHeight: '80vh',
 
-      data: {url : link},
+      data: { url: link },
     });
-    popup.afterClosed().subscribe(item =>{
-      if(item){
-        
+    popup.afterClosed().subscribe(item => {
+      if (item) {
+
       }
     });
   }
@@ -76,43 +97,76 @@ export class MovieListComponent implements OnInit {
     this.dialogRef.closeAll();
   }
 
-  showSynopsisPage(movie:any,upcoming?:any) {
-    let id:any = 1;
-    if(upcoming) {
+  showSynopsisPage(movie: any, upcoming?: any) {
+    let id: any = 1;
+    if (upcoming) {
       id = 0;
-    } 
+    }
     this.router.navigate(['/synopsis', movie.title, id]);
-    
+
   }
 
-  bookTicketsPage(movie:any) {
-    let email = (localStorage.getItem("loggedIn")); 
-    if( email == undefined || email == "false" ) {
+  bookTicketsPage(movie: any) {
+    let email = (localStorage.getItem("loggedIn"));
+    if (email == undefined || email == "false") {
       this.sucessPopup("Please Login to Book Tickets.");
       this.router.navigateByUrl("/home");
     } else {
       this.router.navigate(['/bookTickets', movie.title]);
+      this.router.navigate(['/bookTickets', movie.title]);
     }
   }
-
-  sucessPopup(msg:any) {
+  sucessPopup(msg: any) {
     const popup2 = this.dialogRef.open(MsgPopupHomeComponent, {
       disableClose: true,
       enterAnimationDuration: '700ms',
-      exitAnimationDuration:'1000ms',
+      exitAnimationDuration: '1000ms',
       maxHeight: '80vh',
       width: '400px',
       data: msg
     });
-    popup2.afterClosed().subscribe(item =>{
-      
+    popup2.afterClosed().subscribe(item => {
+
     });
   }
 
-  currentPlayingFilter() {
-    this.searchComp = false;
-    this.searching = false;
-    this.currentPlaying = true;
+currentPlayingFilter() {
+  this.searchComp = false;
+  this.searching = false;
+  this.currentPlaying = true;
+}
+
+
+currentCategory(category:any) {
+  this.currentFilterCategory = category;
+  if(category == "All") {
+    this.currentMovies = this.tempCurrentMovies;
+  } else {
+    this.currentMovies = []
+    for(let i=0;i<this.tempCurrentMovies.length;i++) {
+      for(let j=0;j<this.tempCurrentMovies[i].category.length;j++) {
+        if(this.tempCurrentMovies[i].category[j] == category) {
+          this.currentMovies.push(this.tempCurrentMovies[i]);
+        }
+      }
+    } 
   }
+}
+
+upcomingCategory(category:any) {
+  this.upcomingFilterCategory = category;
+  if(category == "All") {
+    this.upcomingMovies = this.tempUpcomingMovies;
+  } else {
+    this.upcomingMovies = []
+    for(let i=0;i<this.tempUpcomingMovies.length;i++) {
+      for(let j=0;j<this.tempUpcomingMovies[i].category.length;j++) {
+        if(this.tempUpcomingMovies[i].category[j] == category) {
+          this.upcomingMovies.push(this.tempUpcomingMovies[i]);
+        }
+      }
+    } 
+  }
+}
 
 }
